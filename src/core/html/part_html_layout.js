53a7434bdc9c407d5dcaad293583b1c703127a93
U-Keyang-PC\Keyang <Keyang@Keyang-PC.(none)>
2012-02-07 23:46:21 +0000

    add abstract config

--- src/core/html/part_html_layout.js
+++ src/core/html/part_html_layout.js
@@ -1,3 +1,20 @@
 /**
  * Extension of html layout
+ * ./html/part_html_layout.js
  */
+
+mvc.ext(mvc.html, "_layout", mvc.Class.create({
+	defaultLayout:"default",
+	layoutPath:"./app/layout",
+	loadLayout:function(name){
+		var url=this.layoutPath+"/"+name+".html";
+		var content=mvc.html.ajax.syncLoad(url);
+	},
+	initialise : function() {
+		if(!mvc.html.domViewMgr) {
+			mvc.log.e("html view manager is not initialised yet.");
+			return;
+		}
+	}
+	
+}))
---
+++ src/core/part_cfg.js
@@ -0,0 +1,32 @@
+/**
+ * Abstract config class
+ * ./part_cfg.js
+ */
+
+mvc.ext(mvc,"cls",mvc.Class.create({
+	props:{
+		name:"undefined",
+		checkItems:{}
+	},
+	initialise:function(name){
+		this.props.name=name;
+		
+	},
+	check:function(opt){
+		for (var key in this.props.checkItems){
+			var func=this.props.checkItems[key];
+			func(opt);
+		}
+	},
+	addCheckItem:function(name,func){
+		if (typeof name==="string" && typeof func ==="function"){
+			this.props.checkItems[name]=func;
+		}
+	},
+	removeCheckItem:function(name){
+		if (this.props.checkItems[name]){
+			delete this.props.checkItems[name]
+		};
+	}
+}));
+
