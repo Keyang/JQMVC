39b6b1da3b5398c0d82d9c04c178e82c8ae06ad6
Keyang Xiang <keyang.xiang@gmail.com>
2012-02-11 23:16:13 +0000

    updated configuration

--- bin/jqmvc_html.min.js
+++ bin/jqmvc_html.min.js
@@ -13,8 +13,8 @@
 _app_={ 
   ajax:{}, //ajax configurations. checkout jqueyr ajax parameter.
   appContainer:"#pages", //the element in which the app will be rendered. it could be any jquery selector. 
-  elementPath:"app/view/elements", //the absolute path of element folder
-  pagePath:"app/view/pages", // the absolute path of page folder
+  elementPath:"app/elements", //the absolute path of element folder
+  pagePath:"app/view", // the absolute path of page folder
   interfaces:{ //User-defined implementation according to their descriptions. They are affected by UI library chosen.
     /**
      * This is invoked when viewer finished initilising loading page and ask UI to render page
