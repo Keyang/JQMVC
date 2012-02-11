9f3c6f98508ba9cc498b996f9b28c760fbc9dc08
Keyang Xiang <keyang.xiang@gmail.com>
2012-02-11 22:21:56 +0000

    remove readme

---
+++ src/core/html/part_permenant_link.js
@@ -0,0 +1,61 @@
+/**
+ * Add permenant link supporting.
+ * access of a static link is considered as a user action in this framework.
+ * ./html/part_permenant_link.js
+ */
+
+$(document).ready(function() {
+	var hrefStr = window.location.href;
+	var conStr = null;
+	var actStr = null;
+	var params = [];
+	var s1 = hrefStr.indexOf('_ctl=');
+	if(s1 > 0) {
+		var subStr = hrefStr.substr(s1 + 5);
+		var e1 = subStr.indexOf("&");
+		if(e1 == -1) {
+			conStr = subStr;
+			e1 = subStr.indexOf("#");
+			if(e1 != -1) {
+				conStr = subStr.substring(0, e1);
+			}
+		} else {
+			conStr = subStr.substring(0, e1);
+		}
+		var s2 = hrefStr.indexOf('_act=');
+		if(s2 > 0) {
+			subStr = hrefStr.substr(s2 + 5);
+			var e2 = subStr.indexOf("&");
+			if(e2 == -1) {
+				actStr = subStr;
+				e2 = subStr.indexOf("#");
+				if(e2 != -1) {
+					actStr = subStr.substring(0, e2);
+				}
+			} else {
+				actStr = subStr.substring(0, e2);
+			}
+			var s3 = hrefStr.indexOf('_param=');
+			if(s3 > 0) {
+				subStr = hrefStr.substr(s3 + 7);
+				var e2 = subStr.indexOf("&");
+				var paramStr = "[]";
+				if(e2 == -1) {
+					paramStr = subStr;
+					e2 = subStr.indexOf("#");
+					if(e2 != -1) {
+						paramStr = subStr.substring(0, e2);
+					}
+				} else {
+					paramStr = subStr.substring(0, e2);
+				}
+				params = eval("(" + paramStr + ")");
+			}
+			mvc.ctl(conStr).sendMsg(actStr, params);
+		}else{
+			mvc.log.i("_act is not found in static link");
+		}
+
+	}
+
+});
