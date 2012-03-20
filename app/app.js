e09a6289aa7d898f24d5844556a7640ed8e6285b
Keyang Xiang <kxiang@Keyangs-MacBook-Pro.local>
2012-03-20 11:57:03 +0000

    first commit

---
+++ app/app.js
@@ -0,0 +1,14 @@
+/**
+ * app.js contains app initialisation parameters
+ * 
+ * It contains code like:
+ * 
+ * mvc.app.init({
+ * 		launch:function(){
+ * 			alert("launched");
+ * 		}	
+ * 
+ * })
+ * 
+ */
+
